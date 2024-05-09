d638f0d1ef3152036984bc40b8b32150f7424c34
AishwaryashreeTM <160224489+AishwaryashreeTM@users.noreply.github.com>
2024-05-09 16:02:01 +0530

    new file:   .vscode/launch.json new file:   .vscode/settings.json new file:   ani-backend-develop/.github/workflows/build-deploy.yml new file:   ani-backend-develop/.gitignore new file:   ani-backend-develop/.mvn/wrapper/maven-wrapper.jar new file:   ani-backend-develop/.mvn/wrapper/maven-wrapper.properties new file:   ani-backend-develop/README.md new file:   ani-backend-develop/mvnw new file:   ani-backend-develop/mvnw.cmd new file:   ani-backend-develop/pom.xml new file:   ani-backend-develop/src/main/java/com/ani/backend/BackendApplication.java new file:   ani-backend-develop/src/main/java/com/ani/backend/controllers/PropertyAPI.java new file:   ani-backend-develop/src/main/java/com/ani/backend/controllers/PropertyController.java new file:   ani-backend-develop/src/main/java/com/ani/backend/dao/Property.java new file:   ani-backend-develop/src/main/java/com/ani/backend/repositories/PropertyRepository.java new file:   ani-backend-develop/src/main/resources/application.properties new file:   ani-backend-develop/src/test/java/com/ani/backend/BackendApplicationTests.java

---
+++ ani-backend-develop/src/main/java/com/ani/backend/controllers/PropertyController.java
@@ -0,0 +1,81 @@
+package com.ani.backend.controllers;
+
+import com.ani.backend.dao.Property;
+import com.ani.backend.repositories.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class PropertyController {
+
+    @Autowired
+    PropertyRepository propertyRepository;
+
+  
+
+    @GetMapping("/Property/{id}")
+    public ResponseEntity<Property> getPropertyById(@PathVariable Long id) {
+        Optional<Property> propertyObj = propertyRepository.findById(id);
+        if (propertyObj.isPresent()) {
+            return new ResponseEntity<>(propertyObj.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/Property")
+    public ResponseEntity<Property> addProperty(@RequestBody Property property) {
+        try {
+            Property propertyObj = propertyRepository.save(property);
+            return new ResponseEntity<>(propertyObj, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/Property/{id}")
+    public ResponseEntity<Property> updatePeoperty(@PathVariable Long id, @RequestBody Property property) {
+        try {
+            Optional<Property> propertyData = propertyRepository.findById(id);
+            if (propertyData.isPresent()) {
+                Property updatedPropertyData = propertyData.get();
+                updatedPropertyData.setAmenities(property.getAmenities());
+                updatedPropertyData.setBalcony(property.getBalcony());
+                updatedPropertyData.setBathrooms(property.getBathrooms());
+                updatedPropertyData.setBedrooms(property.getBedrooms());
+                updatedPropertyData.setDescription(property.getDescription());
+                updatedPropertyData.setFlatNo(property.getFlatNo());
+                updatedPropertyData.setKitchen(property.getKitchen());
+                updatedPropertyData.setPropertyType(property.getPropertyType());
+                updatedPropertyData.setTitle(property.getTitle());
+                updatedPropertyData.setBuildingId(property.getBuildingId());
+                updatedPropertyData.setMaxguestAllowes(property.getMaxguestAllowes());
+
+Property propertyObj = propertyRepository.save(updatedPropertyData);
+                return new ResponseEntity<>(propertyObj, HttpStatus.CREATED);
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/Property/{id}")
+    public ResponseEntity<Property> deletePropertyById(@PathVariable Long id) {
+        Optional<Property> propertyObj = propertyRepository.findById(id);
+        if (propertyObj.isPresent()) {
+            propertyRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
---
+++ ani-backend-develop/src/main/java/com/ani/backend/repositories/PropertyRepository.java
@@ -0,0 +1,14 @@
+package com.ani.backend.repositories;
+
+import com.ani.backend.dao.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+
+
+}
+
