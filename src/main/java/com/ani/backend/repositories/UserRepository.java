7ddc048ef1624becb1af2cf52df27f61d104403d
gaganrdev <gaganrdev@gmail.com>
2024-05-21 14:03:42 +0000

    feat:Custom method to check existing email and delete one

--- src/main/java/com/ani/backend/repositories/UserRepository.java
+++ src/main/java/com/ani/backend/repositories/UserRepository.java
@@ -3,8 +3,11 @@ package com.ani.backend.repositories;
 import com.ani.backend.dao.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, String> {
     User findByEmail(String email);
+    
+    boolean existsByEmail(String email);
 
+    void deleteByEmail(String email);
 }
 
