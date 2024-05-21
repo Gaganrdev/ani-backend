cf5e5cf41518a159e928430a9fda0599133e9b2c
gaganrdev <gaganrdev@gmail.com>
2024-05-21 14:06:31 +0000

    feat:Two new methods: - newId method to create UUID value for userId - create method to create new user and set newid and creation date

--- src/main/java/com/ani/backend/service/UserService.java
+++ src/main/java/com/ani/backend/service/UserService.java
@@ -2,6 +2,10 @@ package com.ani.backend.service;
 
 import com.ani.backend.dao.User;
 import com.ani.backend.repositories.UserRepository;
+
+import java.util.Date;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +22,16 @@ public class UserService {
         } else {
             throw new RuntimeException("User not found with email: " + email);
         }
+    }
+    private String newId() {
+        return UUID.randomUUID().toString();
+    }
+    public User createUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("User with this email already exists");
+        }
+        user.setUserId(newId());
+        user.setUserCreationDate(new Date());
+        return userRepository.save(user);
     }
 }
