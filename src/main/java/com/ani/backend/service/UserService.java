ae9fad11042bc36dc2534b41b8ab60a450390d30
gaganrdev <gaganrdev@gmail.com>
2024-05-23 14:00:58 +0000

    feat:implement methods to validate otp with 120s limit

--- src/main/java/com/ani/backend/service/UserService.java
+++ src/main/java/com/ani/backend/service/UserService.java
@@ -1,8 +1,10 @@
 package com.ani.backend.service;
 
 import com.ani.backend.dao.User;
+import com.ani.backend.response.user.UserResponse;
 import com.ani.backend.repositories.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.UUID;
 
@@ -34,4 +36,36 @@ public class UserService {
         user.setUserCreationDate(new Date());
         return userRepository.save(user);
     }
+    public User validateOtp(String email, String otp) {
+        @SuppressWarnings("null")
+        User user = userRepository.findById(email).orElse(null);
+        if (user != null && user.getOtp().equals(otp)) {
+          if (isOtpValid(user.getOtpExpiry())) {
+            return user;
+          } else {
+            return null;
+          }
+        }
+        return null;
+      }
+      public UserResponse validateOtpWithDetails(String email, String otp) {
+      User user = validateOtp(email, otp); 
+      if (user != null) {
+        UserResponse userResponse = new UserResponse();
+        userResponse.setUserId(user.getUserId());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setPhoneNumber(user.getPhoneNumber());
+        userResponse.setUserType(user.getUserType());
+     
+        return userResponse;
+      }
+      return null;
+    }
+      private boolean isOtpValid(LocalDateTime otpExpiry) {
+        if (otpExpiry != null) {
+          return otpExpiry.plusSeconds(120).isAfter(LocalDateTime.now());
+        } else {
+          return false; 
+        }
+      }
 }
