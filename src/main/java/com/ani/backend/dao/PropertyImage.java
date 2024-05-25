fbdeb386289974ed147e85bed0e45198c50ba3f4
gaganrdev <gaganrdev@gmail.com>
2024-05-25 12:43:05 +0000

    Refactor:camel cassing and length for imag url

--- src/main/java/com/ani/backend/dao/PropertyImage.java
+++ src/main/java/com/ani/backend/dao/PropertyImage.java
@@ -2,25 +2,30 @@ package com.ani.backend.dao;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "property_image")
+@Data
 public class PropertyImage {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "property_image_id")
     private int propertyImageId;
 
     @Column(name = "property_id")
     private int propertyId;
 
-    @Column(name = "image_url")
-    private String image_url;
+    @Column(name = "image_url",length = 2000)
+    private String imageUrl;
 
     @Column(name = "image_type")
-    private String image_type;
+    private String imageType;
 
     @Column(name = "order")
     private String order;
