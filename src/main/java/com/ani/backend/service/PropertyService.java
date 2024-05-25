08781609f49702131692c556c8076d416c8cc05c
gaganrdev <gaganrdev@gmail.com>
2024-05-25 13:09:47 +0000

    feat:Implement method to find property details and the average ratings

--- src/main/java/com/ani/backend/service/PropertyService.java
+++ src/main/java/com/ani/backend/service/PropertyService.java
@@ -1,7 +1,12 @@
 package com.ani.backend.service;
 
 import com.ani.backend.dao.Property;
+import com.ani.backend.dao.PropertyImage;
+import com.ani.backend.dao.PropertyReview;
 import com.ani.backend.repositories.IPropertyRepository;
+import com.ani.backend.repositories.PropertyImageRepository;
+import com.ani.backend.repositories.PropertyReviewRepository;
+import com.ani.backend.response.property.PropertyResponse;
 import com.ani.backend.response.property.PropertyThumbnailResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,12 +15,19 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PropertyService {
 
     @Autowired
     private IPropertyRepository propertyRepository;
+
+    @Autowired
+    private PropertyImageRepository propertyImageRepository;
+
+    @Autowired
+    private PropertyReviewRepository propertyReviewRepository;
 
     public List<Property> getAllProperties(){
         return propertyRepository.findAll();
@@ -42,4 +54,30 @@ public class PropertyService {
             }
         }
     }
+    public PropertyResponse getPropertyDetailsById(Integer id) {
+        
+        Property property = propertyRepository.findById(id).orElse(null);
+        if (property == null) {
+            return null;
+        }
+
+        List<String> imageUrls = propertyImageRepository.findByPropertyId(id).stream()
+                .map(PropertyImage::getImageUrl)
+                .collect(Collectors.toList());
+
+        Double averageRating = propertyReviewRepository.findByPropertyId(id).stream()
+                .mapToDouble(PropertyReview::getRating)
+                .average()
+                .orElse(0.0);
+
+        return new PropertyResponse(
+                property.getTitle(),
+                property.getPropertyType(),
+                property.getLocation(),
+                property.getDescription(),
+                property.getNearByLocation(),
+                averageRating,
+                imageUrls);
+    }
+    
 }
